51992052c45605d00e66e4e30031451b3a399c82
Harry-III <L849106064@126.com>
2017-05-17 19:34:58 +0800

    更改线程，随点随停
    主要修改HeartView类，添加一个单例线程，修正业务逻辑。

--- app/src/main/java/com/hc/testheart/HeartView.java
+++ app/src/main/java/com/hc/testheart/HeartView.java
@@ -5,37 +5,83 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v4.view.MotionEventCompat;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
- * Package com.hc.testheart
- * Created by HuaChao on 2016/5/25.
+ * Harry 2017/5/17
  */
 public class HeartView extends SurfaceView implements SurfaceHolder.Callback {
-    SurfaceHolder surfaceHolder;
-    int offsetX;
-    int offsetY;
+    private SurfaceHolder surfaceHolder;
+    private int offsetX;
+    private int offsetY;
     private Garden garden;
     private int width;
     private int height;
     private Paint backgroundPaint;
-    private boolean isDrawing = false;
+    private static boolean isDrawing = false;
     private Bitmap bm;
     private Canvas canvas;
     private int heartRadio = 1;
+    private ExecutorService singleThread;
+    private ArrayList<Bloom> blooms = new ArrayList<>();  //花瓣
+    private static float angle = 10;
 
     public HeartView(Context context) {
-        super(context);
-        init();
+        this(context, null);
     }
 
     public HeartView(Context context, AttributeSet attrs) {
         super(context, attrs);
         init();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_UP) {
+            reDraw();
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        singleThread.shutdown(); //当时视图消失的时候，关闭线程
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        this.width = width;
+        this.height = height;
+        //我的手机宽度像素是1080，发现参数设置为30比较合适，这里根据不同的宽度动态调整参数
+        heartRadio = width * 30 / 1080;
+
+        offsetX = width / 2;
+        offsetY = height / 2 - 100;
+        bm = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        canvas = new Canvas(bm);
+
+        drawOnSingleThread();
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 
     private void init() {
@@ -44,23 +90,63 @@ public class HeartView extends SurfaceView implements SurfaceHolder.Callback {
         garden = new Garden();
         backgroundPaint = new Paint();
 //        backgroundPaint.setColor(Color.rgb(0xff, 0xff, 0xe0));
-        backgroundPaint.setColor(Color.WHITE);   //设置背景颜色
+        backgroundPaint.setColor(Color.WHITE);   //设置画布的背景
+
+        singleThread = Executors.newSingleThreadExecutor();
     }
 
-    ArrayList<Bloom> blooms = new ArrayList<>();
+    public void reDraw() {
+        if (singleThread.isShutdown()) {
+            singleThread.shutdownNow();
+        }
+        if (angle >= 30) {
+            angle = 10;
+            blooms.clear();
+            drawOnSingleThread();
+        } else {
+            if (isDrawing) {
+                isDrawing = false;
+            } else {
+                drawOnSingleThread();
+            }
+        }
 
-    public Point getHeartPoint(float angle) {
-        float t = (float) (angle / Math.PI);
-        float x = (float) (heartRadio * (16 * Math.pow(Math.sin(t), 3)));
-        float y = (float) (-heartRadio * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t)));
-
-        return new Point(offsetX + (int) x, offsetY + (int) y);
     }
 
+    private void drawOnSingleThread() {       //在单例的线程池中使用线程
+        singleThread.execute(new Runnable() {
+            @Override
+            public void run() {
+                if (isDrawing) return;
+                isDrawing = true;
+                Log.d("asdf", "current Thread:" + Thread.currentThread());
 
-    //绘制列表里所有的花朵
-    private synchronized void drawHeart() {
-        canvas.drawRect(0, 0, width, height, backgroundPaint);
+                while (isDrawing) {
+                    Bloom bloom = getBloom(angle);
+                    if (bloom != null) {
+                        blooms.add(bloom);
+                    }
+                    if (angle >= 30) {
+                        isDrawing = false;
+                        break;
+                    } else {
+                        angle += 0.2;
+                    }
+                    drawHeart();
+                    try {
+                        Thread.sleep(20);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+//                isDrawing = false;
+            }
+        });
+
+    }
+
+    private void drawHeart() {
+        canvas.drawRect(0, 0, width, height, backgroundPaint); //每个花朵的背景
         for (Bloom b : blooms) {
             b.draw(canvas);
         }
@@ -71,60 +157,6 @@ public class HeartView extends SurfaceView implements SurfaceHolder.Callback {
         surfaceHolder.unlockCanvasAndPost(c);
 
     }
-
-    public synchronized void reDraw() {
-        // TODO: 2017/5/14 1.判断花朵是否在画;  2.继续叠加的画
-        if (angle >= 30) {
-            blooms.clear();
-            drawOnNewThread();
-        } else {
-            drawOnNewThread();
-        }
-
-
-    }
-
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-
-    }
-
-    private static float angle = 10;
-
-    //开启一个新线程绘制
-    private void drawOnNewThread() {
-        new Thread() {
-            @Override
-            public void run() {
-                if (isDrawing) return;
-                isDrawing = true;
-
-//                float angle = 10;
-                while (true) {
-
-                    Bloom bloom = getBloom(angle);
-                    if (bloom != null) {
-                        blooms.add(bloom);
-                    }
-                    if (angle >= 30) {
-                        angle = 10;
-                        break;
-                    } else {
-                        angle += 0.2;
-                    }
-                    drawHeart();
-                    try {
-                        sleep(20);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                isDrawing = false;
-            }
-        }.start();
-    }
-
 
     private Bloom getBloom(float angle) {
 
@@ -152,29 +184,11 @@ public class HeartView extends SurfaceView implements SurfaceHolder.Callback {
         return null;
     }
 
-    @Override
-    public void surfaceCreated(SurfaceHolder holder) {
+    public Point getHeartPoint(float angle) {
+        float t = (float) (angle / Math.PI);
+        float x = (float) (heartRadio * (16 * Math.pow(Math.sin(t), 3)));
+        float y = (float) (-heartRadio * (13 * Math.cos(t) - 5 * Math.cos(2 * t) - 2 * Math.cos(3 * t) - Math.cos(4 * t)));
 
-
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
-        this.width = width;
-        this.height = height;
-        //我的手机宽度像素是1080，发现参数设置为30比较合适，这里根据不同的宽度动态调整参数
-        heartRadio = width * 30 / 1080;
-
-        offsetX = width / 2;
-        offsetY = height / 2 - 55;
-        bm = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
-        canvas = new Canvas(bm);
-        drawOnNewThread();
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder holder) {
-
+        return new Point(offsetX + (int) x, offsetY + (int) y);
     }
 }
